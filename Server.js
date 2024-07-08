986806b2a369dc541ad4869f4523f3d8f7ec920e
Renjith <renjithkrishnan316@gmail.com>
2024-07-08 23:09:31 +0530

    sss

---
+++ Server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+require('dotenv').config()
+var cors=require('cors')
+const userRoute=require('./routes/userRoute')
+
+const dbConfig=require('./config/dbConfig')
+const app=express()
+app.use(cors())
+app.use(express.json())
+app.use('/api/user',userRoute)
+
+
+
+app.listen(7766,()=>{
+    console.log("Server is Running!!!!!!@@@")
+})
+
+app.get('/',(req,res)=>{
+    res.send("HIIIIII@!!!!!")
+})
